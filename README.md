e950160a726b1d8f12c9bcefb1ea9f72be00515d
Robert Peteuil <robert@peteuil.com>
2024-01-26 09:44:55 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# terraform-tls-certificate
+
+a quick terraform module to generate a private key and root certificate to use with the demostack
---
+++ outputs.tf
@@ -0,0 +1,33 @@
+output "ca_key_algorithm" {
+  value = tls_private_key.root.algorithm
+}
+
+output "ca_private_key_pem" {
+  value     = tls_private_key.root.private_key_pem
+  sensitive = true
+}
+
+output "ca_cert_pem" {
+  value = tls_self_signed_cert.root.cert_pem
+}
+
+output "consul_gossip_key" {
+  value = base64encode(random_id.consul_gossip_key.hex)
+}
+
+output "consul_master_token" {
+  value = random_id.consul_master_token.hex
+}
+
+output "consul_join_tag_value" {
+  value = random_id.consul_join_tag_value.hex
+}
+
+output "nomad_gossip_key" {
+  value = base64encode(random_id.nomad_gossip_key.hex)
+}
+
+output "test" {
+  value = base64encode(random_id.nomad_gossip_key.hex)
+}
+
